91effd07a4864b7ef6cf50a2d3f813fd478b3282
MTKS <45343271+tofaelKabir@users.noreply.github.com>
2021-03-26 01:37:29 -0400

    Update PaginationHelper.java

--- src/paginationHelper/PaginationHelper.java
+++ src/paginationHelper/PaginationHelper.java
@@ -9,7 +9,8 @@ public class PaginationHelper {
 		this.arrayOfObj = arrayOfObj;
 		this.itemPerPage = itemPerPage;
 	}
-
+	
+	// This method will return total number of pages
 	public int pageCount() {
 		int pageCounts = 0;
 		try {
@@ -21,10 +22,12 @@ public class PaginationHelper {
 		return pageCounts;
 	}
 
+	// This method will return item count
 	public int itemCount() {
 		return arrayOfObj.length;
 	}
 
+	// This method will take parameter pageNumber and return itemCount
 	public int pageItemCount(int pageNumber) {
 		int itemCount = 0;
 		int totalPageCount = pageCount();
@@ -36,6 +39,7 @@ public class PaginationHelper {
 		return itemCount;
 	}
 
+	// This method will take parameter itemIndex and return pageIndexNo
 	public int pageIndex(int itemIndex) {
 		int pageIndexNo = 0;
 		if (itemIndex > itemCount() || itemIndex < 0) {
